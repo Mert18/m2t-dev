a0a8fc5767f181ea3aaa3b726e11a7c03367b524
Mert <mertuygur02@gmail.com>
2021-05-06 02:35:17 +0300

    of babacım of

--- components/layout/default.js
+++ components/layout/default.js
@@ -5,9 +5,6 @@ import classes from './layout.module.css'
 export default function Default(props) {
     return (
         <div className={classes.lay}>
-            <div className={classes.layhead}>
-                <Header />
-            </div>
             <main className={classes.laymain}>
                 {props.children}
             </main>
