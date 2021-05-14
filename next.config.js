0fdd7ca720e411e613b9aeff804b23e0577e3a3c
Mert <mertuygur02@gmail.com>
2021-05-15 00:38:43 +0300

    spotify handling 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     env: {
       MONGODBURL : process.env.MONGODBURL,
-      URL : process.env.URL
+      URL : process.env.URL,
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+      SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+      TOP_TRACKS: process.env.TOP_TRACKS
     }
   };
