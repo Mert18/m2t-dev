ab8e4215f0a4dbf9d57d69ef1637a41254d85e89
Mert <mertuygur02@gmail.com>
2021-05-15 20:14:46 +0300

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,19 @@ import LayoutWrapper from "../components/layout/layout-wrapper";
 function MyApp({ Component, pageProps }) {
 
     return (
+        <>
+        <Head>
+                <title>Mert Dev</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                <meta property="og:url" content="https://merd.dev" key="ogurl" />
+                <meta property="og:title" content="Mert Dev Web Developer Blog" key="ogtitle" />
+                <meta property="og:description" content="Full stack developer web blog page, writing about technology, web and everything." key="ogdesc" />
+        </Head>
         <LayoutWrapper {...pageProps}>
                 <Component {...pageProps} />
         </LayoutWrapper>
+        </>
     );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,10 @@ class MyDocument extends Document {
                 <Head>
                     <title>Mert Dev</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                    <meta name="description" content="Full stack developer web blog page, writing about technology, web and everything."></meta>
+
+                    <meta property="og:url" content="https://merd.dev" key="ogurl" />
+                    <meta property="og:title" content="Mert Dev Web Developer Blog" key="ogtitle" />
+                    <meta property="og:description" content="Full stack developer web blog page, writing about technology, web and everything." key="ogdesc" />
                 </Head>
                 <body>
                     <Main />
