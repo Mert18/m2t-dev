655c9cd9b19e7b9454adfe66adbf43f9991b1c58
Mert <mertuygur02@gmail.com>
2021-05-23 11:11:57 +0300

    t

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={classes.header}>
             <div className={classes.header__title}>
                 <div className={classes.linkbox}>
-                    <Link href="/"><span data-content="Link Hover" aria-hidden="true"></span>Mert Uygur</Link>
+                    <Link href="/">Mert Uygur</Link>
                 </div>
             </div>
 
@@ -20,19 +20,19 @@ const Header = () => {
 
             <div className={classes.header__nav}>
                 <div className={classes.linkbox}>
-                    <Link href="/tmi"><span data-content="Link Hover" aria-hidden="true"></span>TMI</Link>
+                    <Link href="/tmi">TMI</Link>
                 </div>
 
                 <div className={classes.linkbox}>
-                    <Link href="/posts"><span data-content="Link Hover" aria-hidden="true"></span>BLOG</Link>
+                    <Link href="/posts">BLOG</Link>
                 </div>
                 
                 <div className={classes.linkbox}>
-                    <Link href="/about"><span data-content="Link Hover" aria-hidden="true"></span>ABOUT</Link>
+                    <Link href="/about">ABOUT</Link>
                 </div>
 
                 <div className={classes.linkbox}>
-                    <Link href="/projects"><span data-content="Link Hover" aria-hidden="true"></span>PROJECTS</Link>
+                    <Link href="/projects">PROJECTS</Link>
                 </div>
             </div>
             
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,78 +4,76 @@ import classes from './projects.module.css';
 const Projects = () => {
     return (
         <div className={classes.projects}>
-                <div className={classes.projects__title}>
-                    <h2>Projects</h2>
-                </div>
+            <div className={classes.projects__title}>
+                <h2>Projects</h2>
+            </div>
 
-                <div className={classes.projects__projects}>
-
-                        <div className={classes.project}>
-
-                        <div className={classes.project__text}>
-                            <div className={classes.project__title}>
-                                <h2>From The Heap</h2>
-                            </div>
-                            <div className={classes.project__exp}>
-                                <h3>Share your favorite songs and see others'. Social media platform for those who love sharing things.</h3>
-                            </div>
+            <div className={classes.projects__projects}>
+                <div className={classes.project}>
+                    <div className={classes.project__text}>
+                        <div className={classes.project__title}>
+                            <h2>From The Heap</h2>
                         </div>
 
-                        <div className={classes.project__img}>
-                            <img src="/images/assets/fth.png" width="300px" alt="from the heap site" />
+                        <div className={classes.project__exp}>
+                            <h3>Share your favorite songs and see others'. Social media platform for those who love sharing things.</h3>
                         </div>
-
-                        <div className={classes.project__tech}>
-                                <div className={classes.project__tech__item}>
-                                    <img src="/images/icons/sass-1.svg" width="35px" alt="sass icon" />
-                                </div>
-                                <div className={classes.project__tech__item}>
-                                    <img src="/images/icons/react-2.svg" width="35px" alt="react icon" />
-                                </div>
-                                <div className={classes.project__tech__item}>
-                                    <img src="/images/icons/nodejs-icon.svg" width="35px" alt="node icon" />
-                                </div>
-                                <div className={classes.project__tech__item}>
-                                    <img src="/images/icons/mongodb.svg" width="105px" alt="mongodb icon" />
-                                </div>
-                            </div>
                     </div>
 
-                        <div className={classes.project}>
+                    <div className={classes.project__img}>
+                        <img src="/images/assets/fth.png" width="300px" alt="from the heap site" />
+                    </div>
 
-                            <div className={classes.project__text}>
-                                <div className={classes.project__title}>
-                                    <h2>Virtual Library</h2>
-                                </div>
-                                <div className={classes.project__exp}>
-                                    <h3>Share quotes from books, save your books and your thoughts after you read.</h3>
-                                </div>
-
-                                
-                            </div>
-
-                            <div className={classes.project__img}>
-                                <img src="/images/assets/virtual.png" width="300px" alt="from the heap site" />
-                            </div>
-
-                            <div className={classes.project__tech}>
-                                    <div className={classes.project__tech__item}>
-                                        <img src="/images/icons/sass-1.svg" width="35px" alt="sass icon" />
-                                    </div>
-                                    <div className={classes.project__tech__item}>
-                                        <img src="/images/icons/react-2.svg" width="35px" alt="react icon" />
-                                    </div>
-                                    <div className={classes.project__tech__item}>
-                                        <img src="/images/icons/nodejs-icon.svg" width="35px" alt="node icon" />
-                                    </div>
-                                    <div className={classes.project__tech__item}>
-                                        <img src="/images/icons/postgresql.svg" width="35px" alt="postgresql icon" />
-                                    </div>
-                                </div>
-
+                    <div className={classes.project__tech}>
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/sass-1.svg" width="35px" alt="sass icon" />
                         </div>
+
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/react-2.svg" width="35px" alt="react icon" />
+                        </div>
+
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/nodejs-icon.svg" width="35px" alt="node icon" />
+                        </div>
+                        
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/mongodb.svg" width="105px" alt="mongodb icon" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={classes.project}>
+                    <div className={classes.project__text}>
+                        <div className={classes.project__title}>
+                            <h2>Virtual Library</h2>
+                        </div>
+                        <div className={classes.project__exp}>
+                            <h3>Share quotes from books, save your books and your thoughts after you read.</h3>
+                        </div> 
+                    </div>
+
+                    <div className={classes.project__img}>
+                        <img src="/images/assets/virtual.png" width="300px" alt="from the heap site" />
+                    </div>
+
+                    <div className={classes.project__tech}>
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/sass-1.svg" width="35px" alt="sass icon" />
+                        </div>
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/react-2.svg" width="35px" alt="react icon" />
+                        </div>
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/nodejs-icon.svg" width="35px" alt="node icon" />
+                        </div>
+                        <div className={classes.project__tech__item}>
+                            <img src="/images/icons/postgresql.svg" width="35px" alt="postgresql icon" />
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
     )
 }
 
