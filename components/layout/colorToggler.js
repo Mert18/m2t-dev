5aff4c489862eb7587f563f11a692d58a0ed21e1
Mert <mertuygur02@gmail.com>
2021-06-14 21:23:09 +0300

    header more colors added and footer is now mroe clear.

--- components/layout/colorToggler.js
+++ components/layout/colorToggler.js
@@ -11,6 +11,9 @@ const colorToggler = () => {
             <button className={classes.y} onClick={(e) => {e.preventDefault(), setColor('first')}}></button>
             <button className={classes.g} onClick={(e) => {e.preventDefault(), setColor('second')}}></button>
             <button className={classes.r} onClick={(e) => {e.preventDefault(), setColor('third')}}></button>
+            <button className={classes.s} onClick={(e) => {e.preventDefault(), setColor('sarcoline')}}></button>
+            <button className={classes.t} onClick={(e) => {e.preventDefault(), setColor('tyrian')}}></button>
+            <button className={classes.v} onClick={(e) => {e.preventDefault(), setColor('viridian')}}></button>
         </>
     )
 }
