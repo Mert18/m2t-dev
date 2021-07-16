57ad89a5cea35e201739e4a6fa59b7dbbe498884
Mert <mertuygur02@gmail.com>
2021-07-17 00:18:50 +0300

    some projects addded

--- components/Illustration.js
+++ components/Illustration.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import classes from './styles/illustration.module.css';
-import Image from 'next/image';
+import React from "react";
+import classes from "./styles/illustration.module.css";
+import Image from "next/image";
 
-const Illustration = ({img, title, date}) => {
-    return (
-            <figure className={classes.figure}>
-                <Image src={img} alt={title} width={1024} height={1024} />
-                <figcaption>{title} , {date}</figcaption>
-            </figure>
-    )
-}
+const Illustration = ({ img, title, date }) => {
+  return (
+    <figure className={classes.figure}>
+      <Image
+        placeholder="blur"
+        src={img}
+        alt={title}
+        width={1024}
+        height={1024}
+      />
+      <figcaption>
+        {title} , {date}
+      </figcaption>
+    </figure>
+  );
+};
 
-export default Illustration
+export default Illustration;
