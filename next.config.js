32bc2cadbd148dc23f573452358eeb39ca6a4950
mert18 <mertuygur02@gmail.com>
2021-09-13 15:23:42 +0300

    a lot of problems and i forgot the logic, decided to start from scratch, will be easier to learn and maintain

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const composePlugins = require("next-compose-plugins");
-const mdxEnhanced = require("next-mdx-enhanced");
-
-module.exports = composePlugins([
-  mdxEnhanced({
-    layoutPath: "./templates",
-  }),
-], {
-  images: {
-    domains: [
-      //domains come here
-    ]
-  }
-});
+module.exports = {
+  reactStrictMode: true,
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,7 @@
-import "../styles/globals.css";
-import Head from "next/head";
-import { MDXProvider } from "@mdx-js/react";
-import React, { Fragment } from "react";
-import Layout from "../layouts/default";
-import CodeBlock from "../components/CodeBlock.js";
-
-const components = {
-  pre: (props) => (
-    <div
-      style={{
-        color: "var(--text1)",
-        fontSize: "1rem",
-        backgroundColor: "var(--flavor)",
-      }}
-      {...props}
-    />
-  ),
-  code: CodeBlock,
-};
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Mert Dev</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Mert Uygur. A Full-Stack Developer's blog, playground or diary. MERN Stack, web technologies."
-        />
-        <meta property="og:url" content="https://merd.dev" key="ogurl" />
-        <meta
-          property="og:title"
-          content="Mert Dev Web Developer Blog"
-          key="ogtitle"
-        />
-        <meta
-          property="og:description"
-          content="Full stack developer web blog page, writing about technology, web and everything."
-          key="ogdesc"
-        />
-      </Head>
-      <Layout>
-        <MDXProvider components={components}>
-          <Component {...pageProps} />
-        </MDXProvider>
-      </Layout>
-    </Fragment>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
