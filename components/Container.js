91a90205b832c550a07fc38486ef2587693b683c
mert18 <mertuygur02@gmail.com>
2021-09-16 08:49:03 +0300

    mobile nav menu

--- components/Container.js
+++ components/Container.js
@@ -12,9 +12,9 @@ export default function Container(props) {
   const { resolvedTheme, setTheme } = useTheme();
   const [currentPath, setCurrentPath] = useState("");
   const router = useRouter();
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
 
   const { height, width } = useWindowDimensions();
-  console.log(width);
   useEffect(() => {
     if (router.asPath === "/") {
       setCurrentPath("HOME");
@@ -24,7 +24,10 @@ export default function Container(props) {
         .toUpperCase();
       setCurrentPath(myroute);
     }
-  }, [router]);
+    if (width > 1100) {
+      setMobileMenuVisible(false);
+    }
+  }, [router, width]);
 
   const { children, ...customMeta } = props;
 
@@ -33,6 +36,10 @@ export default function Container(props) {
     description: `Software Developer, Front-end Developer`,
     type: "website",
     ...customMeta,
+  };
+
+  const mobileMenuVisibleHandler = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
   };
   return (
     <div className={classes.container}>
@@ -51,57 +58,95 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <header className={classes.navbar}>
-        <Link href={router.asPath}>
-          <a>
-            <h1 className={classes.headerpath}>{currentPath}</h1>
-          </a>
-        </Link>
-        <ul className={classes.navbarlist}>
-          <li>
-            <Link href="/">
-              <a>
-                H<span className={classes.remaining}>ome</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/resume">
-              <a>
-                R<span className={classes.remaining}>esume</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <a>
-                B<span className={classes.remaining}>log</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/projects">
-              <a>
-                P<span className={classes.remaining}>rojects</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/tmi">
-              <a>
-                T<span className={classes.remaining}>mi</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/ills">
-              <a>
-                I<span className={classes.remaining}>lls</span>
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </header>
+      {width > 1100 ? (
+        <header className={classes.navbar}>
+          <Link href={router.asPath}>
+            <a>
+              <h1 className={classes.headerpath}>{currentPath}</h1>
+            </a>
+          </Link>
+          <ul className={classes.navbarlist}>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/resume">
+                <a>Resume</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/tmi">
+                <a>Tmi</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/ills">
+                <a>Ills</a>
+              </Link>
+            </li>
+          </ul>
+        </header>
+      ) : (
+        <div
+          onClick={mobileMenuVisibleHandler}
+          className={classes.hamburgermenu}
+        >
+          <div className={classes.hamburgerline}></div>
+          <div className={classes.hamburgerline}></div>
+          <div className={classes.hamburgerline}></div>
+        </div>
+      )}
+      {mobileMenuVisible ? (
+        <header className={classes.mobilemenu}>
+          <ul className={classes.navbarlist}>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/resume">
+                <a>Resume</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/tmi">
+                <a>Tmi</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/ills">
+                <a>Ills</a>
+              </Link>
+            </li>
+          </ul>
+        </header>
+      ) : (
+        ""
+      )}
+
       <main className={classes.main} role="main">
         {children}
       </main>
