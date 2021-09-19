1d59d2c951a006b10b79771197ce31259f26e552
mert18 <mertuygur02@gmail.com>
2021-09-19 13:14:07 +0300

    a few sections done

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,44 @@
 import React from "react";
 import Container from "../components/Container";
+import classes from "../styles/blog.module.css";
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import { postFilePaths, POSTS_PATH } from "../utils/mdxUtils";
+import Link from "next/link";
+import BlogPost from "../components/BlogPost";
 
-const Blog = () => {
-  return <Container></Container>;
+const Blog = ({ posts }) => {
+  return (
+    <Container>
+      <div className={classes.blog}>
+        <div className={classes.blogposthome}>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.filePath}>
+                <BlogPost post={post} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Blog;
+
+export function getStaticProps() {
+  const posts = postFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+    const { content, data } = matter(source);
+
+    return {
+      content,
+      data,
+      filePath,
+    };
+  });
+
+  return { props: { posts } };
+}
--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,53 @@
 import React from "react";
 import Container from "../components/Container";
+import classes from "../styles/resume.module.css";
+
+const technologies = [
+  "HTML",
+  "CSS",
+  "React.js",
+  "Next.js",
+  "MongoDB",
+  "PostgreSQL",
+  "Typescript",
+  "GraphQL",
+];
+
+const tools = ["Git", "Atlassian", "Figma", "Webpack", "Adobe Illustrator"];
 
 const Resume = () => {
-  return <Container>HELLO RESUME</Container>;
+  return (
+    <Container>
+      <div className={classes.resume}>
+        <div className={classes.textpart}>
+          <div className={classes.resumetextpart}>
+            <h2>Introduction</h2>
+            <p>
+              Hi, my name is Mert. I have a broad knowledge on web development
+              but I prefer calling myself a Frontend Developer. I love creating
+              websites.
+            </p>
+          </div>
+          <div className={classes.resumetextpart}>
+            <h2>Technologies</h2>
+            <ul className={classes.techlist}>
+              {technologies.map((tech) => {
+                return <li key={tech}>{tech}</li>;
+              })}
+            </ul>
+          </div>
+          <div className={classes.resumetextpart}>
+            <h2>Tools</h2>
+            <ul className={classes.techlist}>
+              {tools.map((tools) => {
+                return <li key={tools}>{tools}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Resume;
