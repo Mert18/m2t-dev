6799a2d8f265c1f1d2e783a01ace64e88c58aebe
mert18 <mertuygur02@gmail.com>
2021-10-15 00:18:22 +0300

    hackerrank route styled. every living creature will die alone

--- pages/hackerrank/[slug].tsx
+++ pages/hackerrank/[slug].tsx
@@ -4,15 +4,23 @@ import Container from "../../components/Container";
 import classes from "../../styles/hackerrank.module.css";
 import { useEffect } from "react";
 import Prism from "prismjs";
+import { useRouter } from "next/router";
 
 export default function Hackerrank({ file }) {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
+
+  const router = useRouter();
+  const myroute = router.query.slug.toString().replace(/\.js/, "");
   return (
     <Container>
       <div className={classes.hackerrank}>
-        <pre>
+        <div className={classes.hackerrank__title}>
+          <h2>Hackerrank Javascript Solutions</h2>
+          <p>Javascript solution for hackerrank question {myroute}</p>
+        </div>
+        <pre className={classes.hackerrank__code}>
           <code className="language-js">{file}</code>
         </pre>
       </div>
