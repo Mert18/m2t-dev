3c1cf8222c327cf134cbb0f53013b7a378ae05c3
Mert18 <mertuygur02@gmail.com>
2022-02-04 22:33:29 +0300

    frontend engineer

--- components/Container.tsx
+++ components/Container.tsx
@@ -76,7 +76,7 @@ export default function Container(props) {
           <div className={classes.sidebarwrapper}>
             <div className={classes.sidebartitle}>
               <h1>Mert UYĞUR</h1>
-              <p>Frontend Developer</p>
+              <p>Front End Engineer</p>
             </div>
             <nav className={classes.navbar}>
               <ul className={classes.navbarlist}>
