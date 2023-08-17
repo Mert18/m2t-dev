0c47dd42b5e85b254f3eb88be786adc398a6644f
mert18 <mertuygur02@gmail.com>
2023-08-17 22:56:51 +0300

    24: page titles added, metadata added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import BlogPosts from "@/components/blog/BlogPosts";
 
+export const metadata = {
+  title: "m2t.dev - Blog",
+  description: "A Software Engineer Blog",
+};
+
 export default async function Home() {
   return (
     <div>
