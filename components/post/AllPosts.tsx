a2c545900d6ea9a67a3c387f4062a1cc687367ee
mert18 <mertuygur02@gmail.com>
2023-09-01 17:52:00 +0300

    43: post filter.

--- components/post/AllPosts.tsx
+++ components/post/AllPosts.tsx
@@ -1,17 +1,67 @@
-import React from "react";
-import { allPosts } from "@/.contentlayer/generated";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Post, allPosts } from "@/.contentlayer/generated";
 import PostCard from "./PostCard";
+import { postCategories } from "@/lib/PostCategories";
+import CategoryIcon from "./CategoryIcon";
 
-const getPosts = async () => {
-  return allPosts;
+const getPosts = (filter: string[]) => {
+  return allPosts.filter((post) => {
+    if (filter) {
+      return filter.some((cat) => post.category.includes(cat));
+    } else {
+      return true;
+    }
+  });
 };
 
-const AllPosts = async () => {
-  const posts = await getPosts();
+const AllPosts = () => {
+  const [filter, setFilter] = useState<string[]>();
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+
+  const handleFilterChange = (category: string) => {
+    if (filter?.includes(category)) {
+      setFilter((prev) => prev?.filter((cat) => cat !== category));
+    } else {
+      setFilter((prev) => {
+        if (prev) {
+          return [...prev, category];
+        } else {
+          return [category];
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!filter || filter.length === 0) {
+      setFilteredPosts(allPosts);
+      return;
+    }
+    setFilteredPosts(getPosts(filter));
+  }, [filter]);
 
   return (
     <div className="w-full sm:w-2/3 md:w-2/5">
-      {posts.map((post) => {
+      <div className="flex justify-center items-center mb-10">
+        {postCategories.map((category: string) => {
+          return (
+            <div
+              className="transition-all cursor-pointer hover:bg-white_100"
+              style={{
+                transform: filter?.includes(category)
+                  ? "translateY(-10px)"
+                  : "none",
+              }}
+              key={category}
+              onClick={() => handleFilterChange(category)}
+            >
+              <CategoryIcon category={category} showExplanation={false} />
+            </div>
+          );
+        })}
+      </div>
+      {filteredPosts.map((post) => {
         return (
           <PostCard
             key={post.title}
