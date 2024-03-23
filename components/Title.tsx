54ca6eed25bcb2f62035fbc79ce81f8becf4bf08
mert18 <mertuygur02@gmail.com>
2024-03-23 10:09:52 +0300

    64: days closed until fix

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const Title = () => {
   return (
     <div className="my-4 flex flex-col justify-center items-center">
-      <Link href="/alive" className="mt-2">
-        <p className="text-xs">am i alive?</p>
-      </Link>
+      {/* <Link href="/days" className="mt-2">
+        <p className="text-xs">days</p>
+      </Link> */}
     </div>
   );
 };
