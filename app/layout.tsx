b6592014c075d1cd7032611f1af97f25bf09b474
mert18 <mertuygur02@gmail.com>
2024-07-07 11:36:35 +0300

    redirect

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({children}: any) => {
+  return (
+    children
+  )
+}
+
+export default Layout
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+const Page = () => {
+  redirect("/en");
+};
+
+export default Page;
